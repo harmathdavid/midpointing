878eea610d748561967af7428d01f53b67c40082
Harmath Dávid <31950652+harmathdavid@users.noreply.github.com>
2019-04-23 14:26:19 +0200

    init commit

---
+++ intervall_felezo/intervall_felezo/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace intervall_felezo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(660, 380);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown_1);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(10, 407);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Üss <Enter>-t az intervallum felezéshez";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(480, 407);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Üss <Space>-t a nullázáshoz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 442);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ intervall_felezo/intervall_felezo/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace intervall_felezo
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Brush bp = new SolidBrush(Color.Red);
+        Pen pl = new Pen(Color.Blue, 2);
+        Pen line = new Pen(Color.Red);
+        List<PointF> points;
+        Point origo;
+        int iterate;
+        float sx1, sx2;
+        float epsilon = 0.5f;
+
+        public Form1()
+        {
+            InitializeComponent();
+            points = new List<PointF>();
+            origo = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            iterate = 0;
+            this.KeyDown += Form1_KeyDown;
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if(points.Count==2)
+                {
+                    iterate++;
+                    IntervallBisect(iterate);
+                }
+                else
+                {
+                    MessageBox.Show("Kérlek előbb helyezz el a pontokat az egér segítségével");
+                }
+                
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                iterate = 0;
+                Clear();
+                line.Color = Color.Red;
+            }
+        }
+
+        int found = -1;
+        int size = 4;
+        int scale = 20;
+
+
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (points.Count == 2)
+            {
+                if (checkPointsAreValid(points))
+                {
+                    sx1 = points[0].X;
+                    sx2 = points[1].X;
+
+                    pictureBox1.Refresh();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Nincs metszet az X tengellyel");
+                    Clear();
+                }
+
+            }
+
+            found = -1;
+
+        }     
+
+
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (points.Count < 2)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    if (Math.Abs(points[i].X - e.X) < size && Math.Abs(points[i].Y - e.Y) < size)
+                        found = i;
+                }
+
+                if (found == -1)
+                {
+                    points.Add(e.Location);
+                    pictureBox1.Refresh();
+
+                }
+            }
+
+
+        }
+
+        void DrawCoordSys()
+        {
+            g.DrawLine(new Pen(Color.Black), new PointF(0, pictureBox1.Height / 2), new PointF(pictureBox1.Width, pictureBox1.Height / 2));
+            g.DrawLine(new Pen(Color.Black), new PointF(pictureBox1.Width / 2, 0), new PointF(pictureBox1.Width / 2, pictureBox1.Height));
+
+            for (int i = 0; i < pictureBox1.Width; i++)
+            {
+                if (i % scale == 0)
+                    g.DrawRectangle(new Pen(Color.Black), i, (pictureBox1.Height / 2) - 2, 1, 4);
+            }
+            for (int i = 0; i < pictureBox1.Height; i++)
+            {
+                if (i % scale == 0)
+                    g.DrawRectangle(new Pen(Color.Black), (pictureBox1.Width / 2) - 2, i, 4, 1);
+            }
+
+        }
+
+        bool checkPointsAreValid(List<PointF> l)
+        {
+            PointF SPoint1 = position(points[0]);
+            PointF SPoint2 = position(points[1]);
+
+            if (Math.Sign(SPoint1.X) != Math.Sign(SPoint2.X))
+                return true;
+            else return false;
+
+        }
+
+        void Clear()
+        {
+            points.Clear();
+            pictureBox1.Refresh();
+        }
+
+        //2 pontra illeszkedő egyenes egyenlete
+        float basicFunction(PointF A, PointF B, float x)
+        {
+            PointF V = new PointF(B.X - A.X, B.Y - A.Y);
+
+            return ((V.Y * A.X - V.X * A.Y) - V.Y * x) / -(V.X);
+        }
+
+        float fx(float x)
+        {
+            return basicFunction(points[0], points[1], x);
+        }
+
+
+        void IntervallBisect(int iterations)
+        {
+            float c;
+            
+            c = (sx1+sx2) / 2;
+
+            if (position(fx(sx1)) * position(fx(c))< 0)
+                sx2 = c;
+            else
+                sx1 = c;
+
+
+            if (Math.Abs(sx1 - sx2) < epsilon)
+            {
+                line.Color = Color.Green;
+            }
+
+            pictureBox1.Refresh();
+        }
+        float position(float A)
+        {
+            if (A < origo.Y)
+            {
+                return A * (-1);
+            }
+            else
+            {
+                return A;
+            }
+
+        }
+        
+        PointF position(PointF A)
+        {
+            if (A.X < origo.X && A.Y < origo.Y)
+                return new PointF(A.X * (-1), A.Y);
+            else if (A.X > origo.X && A.Y < origo.Y)
+                return new PointF(A.X * (-1), A.Y * (-1));
+            else if (A.X > origo.X && A.Y > origo.Y)
+                return new PointF(A.X, A.Y * (-1));
+            else
+                return A;
+        }
+
+        
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+          
+            DrawCoordSys();
+
+            if (points.Count == 2)
+            {
+               
+                g.DrawLine(line, new PointF(Math.Abs(sx1), 0), new PointF(Math.Abs(sx1), pictureBox1.Height));
+                g.DrawLine(line, new PointF(Math.Abs(sx2), 0), new PointF(Math.Abs(sx2), pictureBox1.Height));
+
+                g.DrawLine(new Pen(Color.Blue), points[0], points[1]);
+            }
+
+            foreach (var p in points)
+            {
+                g.FillEllipse(bp, p.X - size, p.Y - size, size * 2, size * 2);
+            }
+        }
+
+
+    }
+}
+
